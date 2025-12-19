03ea8575c2468b1c26d7caaaa1b2bc35b3574743
JT Raber <JMANN240raber@gmail.com>
2025-12-19 14:47:29 -0500

    started DRY

--- src/can/frame/data/mod.rs
+++ src/can/frame/data/mod.rs
@@ -1,2 +1,23 @@
+use bitvec::prelude::*;
+
+use crate::can::frame::CANFrameDecodingError;
+
 pub mod base;
 pub mod extended;
+
+fn extract_field<U, T, B, F>(
+    bits: B,
+    offset: usize,
+    length: usize,
+    missing_error: CANFrameDecodingError,
+    load: F,
+) -> Result<U, CANFrameDecodingError>
+where
+    T: BitStore,
+    B: AsRef<BitSlice<T, Msb0>>,
+    F: Fn(&BitSlice<T, Msb0>) -> U
+{
+    let maybe_bit_slice = bits.as_ref().get(offset..(offset + length));
+    let bit_slice = maybe_bit_slice.ok_or(missing_error)?;
+    Ok(load(bit_slice))
+}
